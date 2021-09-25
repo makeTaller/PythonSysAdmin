ce11584eceef00a75b51794538ef854b24e34c43
Kirk Tolliver <KirkTolliver28@gmail.com>
2021-09-25 16:31:51 -0500

    Finished working with pathlib

--- adv_pathlib_examples.py
+++ adv_pathlib_examples.py
@@ -7,6 +7,25 @@ mydir = Path(".").absolute()
 mydir.mkdir(parents=True, exist_ok=True)              
 
 
-newDir = mydir / 'library'
 
-newDir.mkdir(parents=True)
+# newDir = mydir / 'library'
+
+# newDir.mkdir(parents=True)
+
+print("View code that makes directories with mkdir ")
+
+nfile = Path('./library') / "newfile.txt"
+
+nfile.exists()
+
+print(nfile.exists)
+
+# nfile.mkdir(parents=True,exist_ok=True)
+
+nfile.write_text('Hello Kirk Tollive')
+
+print( "Wrote file and file content with file write_File")
+
+read_file = nfile.read_text()
+
+print(f"Read file info with read_text {read_file}")
