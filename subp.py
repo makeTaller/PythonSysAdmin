7c69f27be6e4993e8b65174e40c96f033fada02f
Kirk Tolliver <KirkTolliver28@gmail.com>
2021-09-25 20:02:24 -0500

    Understand how to make subprocesses and to check for errors

--- subp.py
+++ subp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import subprocess
+import shlex
 # import os
 
 # p = os.system("ls")
@@ -23,6 +24,28 @@ command = "cat {}".format(userinput)
 
 newSt = subprocess.run(command, shell=True, capture_output=True)
 print(f"\nSubprocess example of the security flaw {newSt}")
+
+shq = shlex.quote(userinput)
+command1 = "cat {}".format(shlex.quote(userinput))
+print( command1)
+
+# This is how to make a list of a command of text
+
+commandNew = shlex.split("cat 'xyc fer' >> file.txt")
+print(f'This is a list of commmands created by shlex.split {commandNew}')
+
+# file_input = subprocess.run(["python test.py"], capture_output=True, input="abc\ndef", text=True)
+# print(f'This is putting input from user commands into the test.py {file_input}')
+
+# file_input = subprocess.run(["python test.py"], capture_output=True, stdin=open("abc\ndef", "r", text=True)
+
+# this is to make the shell sleep 
+subprocess.run(["sleep","3"], timeout=5)
+
+try:
+    subprocess.run(["ls","-xla"], check=True)
+except subprocess.CalledProcessError:
+    print("failed")
 
 
 # p = subprocess()
